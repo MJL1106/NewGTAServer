0e761689efbb82ad3914ebd97850c5d5cf1c02ea
matt <carman5958@hotmail.com>
2024-02-10 17:20:33 -0500

    Added Cars, Changed legion, Removed store in legion Tested working

---
+++ resources/[vehicles]/Nissan 180SX/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+data_file 'HANDLING_FILE' '180sx/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '180sx/vehicles.meta'
+data_file 'CARCOLS_FILE' '180sx/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '180sx/carvariations.meta'
+data_file 'DLC_TEXT_FILE' '180sx/dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' '180sxcontentunlocks.meta'
+
+files {
+  '180sx/handling.meta',
+  '180sx/vehicles.meta',
+  '180sx/carcols.meta',
+  '180sx/carvariations.meta',
+  '180sx/dlctext.meta',
+  '180sx/180sxcontentunlocks.meta',
+}
+client_script 'vehicle_names.lua'
