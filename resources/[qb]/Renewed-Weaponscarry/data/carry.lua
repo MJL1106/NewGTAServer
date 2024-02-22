714089067c3a72b9ab24d5db28d9d32aeed0a375
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2024-02-22 01:31:42 +0000

    Began House robberies

---
+++ resources/[qb]/Renewed-Weaponscarry/data/carry.lua
@@ -0,0 +1,165 @@
+return {
+    oilbarrel = {
+        model = `prop_barrel_exp_01a`,
+        pos = vec3(0.01, -0.27, 0.27),
+        rot = vec3(3.0, 0.0, 0.0),
+        bone = 28422,
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+    pcequipment = {
+        model = 'prop_dyn_pc_02',
+        bone = 24817,
+        pos = vec3(0.09, 0.43, 0.05),
+        rot = vec3(91.0, 0.0, -265.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    coffeemaker = {
+        model = 'prop_coffee_mac_02',
+        bone = 24817,
+        pos = vec3(0.02, 0.43, 0.03),
+        rot = vec3(91.0, 0.0, -265.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    flight_artpiece = {
+        model = 'v_res_sculpt_decf',
+        bone = 24817,
+        pos = vec3(-0.25, 0.43, 0.00),
+        rot = vec3(0.0, 90.0, 0.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    musicequipment = {
+        model = 'prop_speaker_06',
+        bone = 24817,
+        pos = vec3(-0.10, 0.43, 0.05),
+        rot = vec3(180.0, 0.0, -265.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    microwave = {
+        model = 'prop_microwave_1',
+        bone = 24817,
+        pos = vec3(-0.20, 0.43, 0.05),
+        rot = vec3(91.0, 0.0, -265.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    flight_suitcase = {
+        model = 'prop_ld_suitcase_01',
+        bone = 24817,
+        pos = vec3(-0.10, 0.43, 0.05),
+        rot = vec3(0.0, 90.0, 0.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    flight_television = {
+        model = 'sm_prop_smug_tv_flat_01',
+        bone = 24817,
+        pos = vec3(-0.20, 0.43, 0.0),
+        rot = vec3(0.0, -90.0, 180.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    flight_painting = {
+        model = 'ch_prop_vault_painting_01c',
+        bone = 24817,
+        pos = vec3(-0.18, 0.43, 0.05),
+        rot = vec3(180.0, 90.0, 0.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    flight_painting2 = {
+        model = 'ch_prop_vault_painting_01d',
+        bone = 24817,
+        pos = vec3(-0.18, 0.43, 0.05),
+        rot = vec3(180.0, 90.0, 0.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+
+    flight_guitar = {
+        model = 'sf_prop_sf_guitar_case_01a',
+        bone = 24817,
+        pos = vec3(-0.01, 0.50, 0.05),
+        rot = vec3(90.0, 0.0, 0.0),
+        dict = 'anim@heists@box_carry@',
+        anim = 'idle',
+        disableKeys = {
+            disableSprint = true,
+            disableJump = true,
+            disableAttack = true,
+            disableVehicle = true
+        }
+    },
+}
