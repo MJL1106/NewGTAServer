b78d524e93c0ca6444c5f298eb155c2fca7b1599
GenerousFly <159313880+GenerousFly@users.noreply.github.com>
2024-03-01 16:13:16 -0800

    Logs

--- resources/[qb]/nv_3D-DisconnectReason/config.lua
+++ resources/[qb]/nv_3D-DisconnectReason/config.lua
@@ -1,12 +1,12 @@
 Config = {}
 
 Config.Translation = function(source, name, reason)
-    return source ..name..'~s~ \nREASON: ~r~'..reason;
+    return source.. " " ..name..'~s~ \nREASON: ~r~'..reason;
 end
 
 Config.Reasons = {
     ['timed out'] = 'Timed Out',
     ['crash'] = 'Crashed',
-    ['Exiting'] = 'Quitted',
+    ['Exiting'] = 'Exited',
     ['Disconnected.'] = 'Disconnected',
 }
