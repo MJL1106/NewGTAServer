50a1542dc89a594d2dd975e927bcedecd3751161
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2024-03-02 00:10:18 +0000

    combat parsing

--- resources/[qb]/nv_3D-DisconnectReason/config.lua
+++ resources/[qb]/nv_3D-DisconnectReason/config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Translation = function(source, name, reason)
-    return [..source..] ..name..'~s~ \nREASON: ~r~'..reason;
+    return source ..name..'~s~ \nREASON: ~r~'..reason;
 end
 
 Config.Reasons = {
