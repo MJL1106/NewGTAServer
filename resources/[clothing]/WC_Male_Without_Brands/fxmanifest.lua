aaefa27f4d31f6b0184f490a162ba73806887b2a
MJL1106 <75175998+MJL1106@users.noreply.github.com>
2024-03-06 12:35:47 +0000

    Male clothing

---
+++ resources/[clothing]/WC_Male_Without_Brands/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_wc_nb_male_part1.meta',
+  'mp_m_freemode_01_mp_m_wc_nb_male_part2th.meta',
+  'stream/pedalternatevariations.meta',
+  'stream/pedalternatevariations2.meta',
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_wc_nb_male_part1.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_wc_nb_male_part2th.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations2.meta'
