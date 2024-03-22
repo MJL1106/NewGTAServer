d5e4c7d25fc1a2e553d88bec58d50e83537c2673
GenerousFly <159313880+GenerousFly@users.noreply.github.com>
2024-03-22 10:19:12 -0700

    logs

--- resources/[qb]/s1n_banking/server/config.lua
+++ resources/[qb]/s1n_banking/server/config.lua
@@ -1,4 +1,4 @@
 Config = Config or {}
 
 -- Add the url to your Discord webhook to receive notifications for player transactions
-Config.WebhookLink = ''
+Config.WebhookLink = 'https://discord.com/api/webhooks/1220769231860273152/rKHgfo0_gG1eU22sIYpYMu6RDiFj6GW_PaIVar96q8hJrzQeIVXpMEW3P2P5liwF90Js'
